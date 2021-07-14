6d4a05a58c8e283fff0f180d8297346c9c732af0
smally979 <87448891+smally979@users.noreply.github.com>
2021-07-14 22:41:21 +0100

    Completed module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -23,7 +23,7 @@ top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
 
 #create bar chart
-fix, ax = plt.subplots()
+fig, ax = plt.subplots()
 ax.bar(categories,count)
 ax.set_title('# of Purchases by Category')
 plt.show()
